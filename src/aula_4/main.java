109c406617ce102763e53a62ab90ea92fb2919af
VICTOR MATHEUS ALVES DE OLIVEIRA <victor.maoliveira@senacsp.edu.br>
2018-08-31 22:10:00 -0300

    first commit

---
+++ src/aula_4/main.java
@@ -0,0 +1,11 @@
+package aula_4;
+//@author victor.maoliveira
+public class main {
+    public static void main(String[] args) {
+        
+        JFrame tela1 = new JFrame();
+        tela1.setVisible(true);
+        
+    }
+    
+}
